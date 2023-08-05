ad0e8ab4295b0529b789e5a6de0edb7af1b24898
DianaCh <diana.chaglla.post@gmail.com>
2023-08-05 00:54:49 -0500

    "Act. Script DC"

--- Scripts/moduloR_grupo6.R
+++ Scripts/moduloR_grupo6.R
@@ -11,11 +11,7 @@ balances_2014 <- read.xlsx("Data/proyecto_final/balances_2014.xlsx")
 ciiu <- read.xlsx("Data/proyecto_final/ciiu.xlsx")
 
 #Limpieza de datos ----
-<<<<<<< HEAD
-#imputamos los datos con información faltante 
-=======
 #impute los NAs
->>>>>>> 4e2793696d9f88d38b27347faf24da4d04bbfd24
 balances_2014 <- balances_2014 %>% na.omit()
 
 #exploracion de datos ----
@@ -32,7 +28,6 @@ balances_tbl <- balances_2014 %>% select(nombre_cia,
                                          provincia,
                                          canton,
                                          ciudad,
-                                         tamanio,
                                          ciiu4_nivel1,
                                          ciiu4_nivel6, 
                                          v345,
@@ -43,8 +38,8 @@ balances_tbl <- balances_2014 %>% select(nombre_cia,
 
 #union de tabla
 balances_tbl <- balances_tbl %>% left_join(ciiu, by = c("ciiu4_nivel1" = "CODIGO")) %>%
-                                 left_join(ciiu, by = c("ciiu4_nivel6" = "CODIGO")) %>%
-                                 view()
+  left_join(ciiu, by = c("ciiu4_nivel6" = "CODIGO")) %>%
+  view()
 
 #renaming variables
 balances_tbl <- balances_tbl %>% rename("Activos_Corrientes" = "v345",
@@ -73,42 +68,37 @@ balances_tbl <- balances_tbl %>% na.omit() #vuelvo a omitir NAs
 
 empresas <- as_tibble(balances_tbl)
 
-<<<<<<< HEAD
-=======
 ##Respuestas pregunta 2
 ## Empresas con mayor apalancamiento ----
 Top10_A <- empresas %>% select(nombre_cia, 
-                                   situacion,
-                                   Actividad_Economica, 
-                                   pais,
-                                   ciudad,
-                                   Apalancamiento) %>%
-                            group_by(nombre_cia) %>%
-                            summarise(Total_Apalancamiento = sum(Apalancamiento)) %>%
-                            arrange(desc(Total_Apalancamiento)) %>%
-                            head(n=10)
+                               situacion,
+                               Actividad_Economica, 
+                               pais,
+                               ciudad,
+                               Apalancamiento) %>%
+  group_by(nombre_cia) %>%
+  summarise(Total_Apalancamiento = sum(Apalancamiento)) %>%
+  arrange(desc(Total_Apalancamiento)) %>%
+  head(n=10)
 
 Top10_A %>% ggplot(aes(x = fct_reorder(nombre_cia, Total_Apalancamiento), y = Total_Apalancamiento, fill = nombre_cia)) +
-            geom_col() +
-            coord_flip() +
-            labs(title = "TOP 10 Empresar con Apalancamiento mas alto", x = "Compania", y = "Apalancamiento") +
-            theme(legend.position = 'none')
+  geom_col() +
+  coord_flip() +
+  labs(title = "TOP 10 Empresar con Apalancamiento mas alto", x = "Compania", y = "Apalancamiento") +
+  theme(legend.position = 'none')
 
 ## graficas
 ##Liquidez y solvencia por situacion y provincia
 empresas_plot3 <- empresas %>% group_by(provincia, situacion) %>%
-                               summarise(Liquidez_corrienteT = sum(Liquidez_corriente),
-                                         Endeudamiento_activoT = sum(Endeundamiento_activo),
-                                         Endeudamiento_patrimonialT = sum(Endeudamiento_patrimonial),
-                                         Endeudamiento_del_activo_fijoT = sum(Endeudamiento_del_activo_fijo),
-                                         ApalancamientoT = sum(Apalancamiento)
-                                         )
+  summarise(Liquidez_corrienteT = sum(Liquidez_corriente),
+            Endeudamiento_activoT = sum(Endeundamiento_activo),
+            Endeudamiento_patrimonialT = sum(Endeudamiento_patrimonial),
+            Endeudamiento_del_activo_fijoT = sum(Endeudamiento_del_activo_fijo),
+            ApalancamientoT = sum(Apalancamiento)
+  )
 
 #por terminar
 empresas_plot3 %>% ggplot(aes(x = provincia, y = Liquidez_corrienteT, fill = situacion)) +
-                   geom_col() 
-                                         
+  geom_col() 
+
 ## Natashas Calle ----
-
-
->>>>>>> 4e2793696d9f88d38b27347faf24da4d04bbfd24
