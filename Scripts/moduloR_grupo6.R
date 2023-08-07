1fe362b1f82bb83111ed0f69b05fb53118774730
DianaCh <diana.chaglla.post@gmail.com>
2023-08-06 19:34:45 -0500

    "Final SPart DC"

--- Scripts/moduloR_grupo6.R
+++ Scripts/moduloR_grupo6.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(tidyverse)
 library(readxl)
 library(ggplot2)
+library(patchwork)
 
 #Importacion de datos -----
 balances_2014 <- read.xlsx("Data/proyecto_final/balances_2014.xlsx")
@@ -50,7 +51,9 @@ balances_tbl <- balances_tbl %>% rename("Activos_Corrientes" = "v345",
                                         "Actividad_Economica" = "DESCRIPCION.x",
                                         "Nivel1" = "NIVEL.x",
                                         "Subactividad" = "DESCRIPCION.y",
-                                        "Nivel6" = "NIVEL.y")
+                                        "Nivel6" = "NIVEL.y",
+                                        "Status" = "situacion", 
+                                        "Compania" = "nombre_cia")
 
 #mutate para crear variables 
 balances_tbl <- balances_tbl %>% mutate(Activo = Activos_Corrientes + Activos_NoCorrientes,
@@ -70,35 +73,71 @@ empresas <- as_tibble(balances_tbl)
 
 ##Respuestas pregunta 2
 ## Empresas con mayor apalancamiento ----
-Top10_A <- empresas %>% select(nombre_cia, 
-                               situacion,
+Top10_A <- empresas %>% select(Compania, 
+                               Status,
                                Actividad_Economica, 
                                pais,
                                ciudad,
                                Apalancamiento) %>%
-  group_by(nombre_cia) %>%
+  group_by(Compania) %>%
   summarise(Total_Apalancamiento = sum(Apalancamiento)) %>%
   arrange(desc(Total_Apalancamiento)) %>%
   head(n=10)
 
-Top10_A %>% ggplot(aes(x = fct_reorder(nombre_cia, Total_Apalancamiento), y = Total_Apalancamiento, fill = nombre_cia)) +
+Top10_A %>% ggplot(aes(x = fct_reorder(Compania, Total_Apalancamiento), y = Total_Apalancamiento, fill = Compania)) +
   geom_col() +
   coord_flip() +
-  labs(title = "TOP 10 Empresar con Apalancamiento mas alto", x = "Compania", y = "Apalancamiento") +
+  labs(title = "TOP 10 Empresas con Apalancamiento mas alto", x = "Compania", y = "Apalancamiento") +
   theme(legend.position = 'none')
 
 ## graficas
-##Liquidez y solvencia por situacion y provincia
-empresas_plot3 <- empresas %>% group_by(provincia, situacion) %>%
-  summarise(Liquidez_corrienteT = sum(Liquidez_corriente),
-            Endeudamiento_activoT = sum(Endeundamiento_activo),
-            Endeudamiento_patrimonialT = sum(Endeudamiento_patrimonial),
-            Endeudamiento_del_activo_fijoT = sum(Endeudamiento_del_activo_fijo),
-            ApalancamientoT = sum(Apalancamiento)
-  )
 
-#por terminar
-empresas_plot3 %>% ggplot(aes(x = provincia, y = Liquidez_corrienteT, fill = situacion)) +
-  geom_col() 
+LC <- empresas %>% ggplot(aes(x = provincia, y = Liquidez_corriente, fill = Status)) +
+  geom_bar(stat = "summary", position = "stack") +
+  labs(title = "Comparativo de Liquidez Corriente por Status y Provincia",
+       x = "Provincia", y = "Liquidez Corriente") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(size=5,angle = 45, hjust = 1))
+LC
 
-## Natashas Calle ----
+EA <- empresas %>% ggplot(aes(x = provincia, y = Endeundamiento_activo, fill = Status)) +
+  geom_bar(stat = "summary", position = "stack") +
+  labs(title = "Comparativo de Endeudamiento del Activo por Status y Provincia",
+       x = "Provincia", y = "Endeudamiento del Activo") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(size=5,angle = 45, hjust = 1))
+EA
+
+EP <- empresas %>% ggplot(aes(x = provincia, y = Endeudamiento_patrimonial, fill = Status)) +
+  geom_bar(stat = "summary", position = "stack") +
+  labs(title = "Comparativo de Endeudamiento Patrimonial por Status y Provincia",
+       x = "Provincia", y = "Endeudamiento Patrimonial") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(size=5,angle = 45, hjust = 1))
+EP
+
+EP <- empresas %>% ggplot(aes(x = provincia, y = Endeudamiento_patrimonial, fill = Status)) +
+  geom_bar(stat = "summary", position = "stack") +
+  labs(title = "Comparativo de Endeudamiento Patrimonial por Status y Provincia",
+       x = "Provincia", y = "Endeudamiento Patrimonial") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(size=5,angle = 45, hjust = 1))
+EP
+
+EAF <- empresas %>% ggplot(aes(x = provincia, y = Endeudamiento_del_activo_fijo, fill = Status)) +
+  geom_bar(stat = "summary", position = "stack") +
+  labs(title = "Comparativo de Endeudamiento del Activo Fijo por Status y Provincia",
+       x = "Provincia", y = "Endeudamiento del Activo Fijo") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(size=5,angle = 45, hjust = 1))
+EAF
+
+A <- empresas %>% ggplot(aes(x = provincia, y = Apalancamiento, fill = Status)) +
+  geom_bar(stat = "summary", position = "stack") +
+  labs(title = "Comparativo de Apalancamiento por Status y Provincia",
+       x = "Provincia", y = "Apalancamiento") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(size=5,angle = 45, hjust = 1))
+A
+
+## Natasha Calle ----
